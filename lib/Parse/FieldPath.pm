3078398d595602294fab3a482e4fbd88b2161210
Paul Boyd <boyd.paul2@gmail.com>
2011-09-22 06:44:45 -0400

    Pre-compile the parser
    * bin/rebuild_grammar.pl: Script to generate Parse::FieldPath::Parser.
    * lib/Parse/FieldPath.pm: Use the pre-compiled parser.
    * lib/Parse/FieldPath/Parser.pm: The parser.

--- lib/Parse/FieldPath.pm
+++ lib/Parse/FieldPath.pm
@@ -8,57 +8,14 @@ use warnings;
 use Exporter qw/import unimport/;
 our @EXPORT_OK = qw/build_tree extract_fields/;
 
-use Parse::RecDescent;
 use Scalar::Util;
 use Carp;
 
-sub _parser {
-    my $grammar = q{
-        parse: fields /^\Z/
-            {
-                $return = $item[1];
-            }
-
-        fields: field(s /,/)
-            {
-                use Hash::Merge qw//;
-                use List::Util qw//;
-                $return = List::Util::reduce { Hash::Merge::merge($a, $b) } {}, @{$item[1]};
-            }
-
-        field: field_list | field_path | <error>
-
-        field_name: /\w+/ | '*' | <error?>
-        field_list: field_path '(' fields ')'
-            {
-                sub deepest {
-                    my $hashref = shift;
-                    return $hashref if scalar(keys %$hashref) == 0;
-                    my $key = (keys %$hashref)[0];
-                    return deepest($hashref->{$key});
-                }
-                my $deepest = deepest($item{field_path});
-                $deepest->{$_} = $item{fields}->{$_} for keys %{$item{fields}};
-                $return = $item{field_path};
-            }
-
-        # Matches "a/b", "a/b/c" or just "a"
-        field_path: field_name(s /\//)
-            {
-                use List::Util qw//;
-
-                # Turn qw/a b c/ into { a => { b => { c => {} } } }
-                my $fields = {};
-                List::Util::reduce { $a->{$b} = {} } $fields, @{$item{'field_name(s)'}};
-                $return = $fields;
-            }
-    };
-    return Parse::RecDescent->new($grammar);
-}
+use Parse::FieldPath::Parser;
 
 sub build_tree {
     my ($field_path) = @_;
-    my $parser = _parser;
+    my $parser = Parse::FieldPath::Parser->new();
     return $parser->parse($field_path);
 }
 
