76ec89bb2a4c46918aec4328bb427ee734e3e3d0
Paul Boyd <boyd.paul2@gmail.com>
2011-09-26 22:26:52 -0400

    Role POD
    * lib/Parse/FieldPath/Role.pm: POD

--- lib/Parse/FieldPath/Role.pm
+++ lib/Parse/FieldPath/Role.pm
@@ -14,3 +14,46 @@ sub extract_fields {
 }
 
 1;
+
+=pod
+
+=head1 NAME
+
+Parse::FieldPath::Role
+
+=head1 ABSTRACT
+
+Moose role to provide an C<extract_fields> method.
+
+=head1 SYNOPSIS
+
+  package Boris;
+
+  use Moose;
+
+  has plan => ( is => 'rw' );
+  has tnt => ( is => 'rw' );
+
+  # Meanwhile..
+  my $boris = Boris->new(plan => 'a very evil plan', tnt => 'lots');
+  $boris->extract_fields(""); # returns: {
+                                           plan => 'a very evil plan',
+                                           tnt => 'lots',
+                                         }
+
+=head1 PROVIDED METHODS
+
+=over 4
+
+=item B<all_fields()>
+
+Called by C<extract_fields()> to get a list of every available field for the
+object. This implementation return the name of every attribute reader method.
+
+=item B<extract_fields($field_path)>
+
+Calls C<extract_fields> from L<Parse::FieldPath> on the object.
+
+=back
+
+=cut
